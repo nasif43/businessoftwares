8644b418cc486d82729f39f36114d7e5a846ed26
Muhtasim <muhtasimhossain43@gmail.com>
2025-02-28 11:25:02 +0600

    business softwares

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
 });
